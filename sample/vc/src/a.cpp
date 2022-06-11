257cdcb120dc85daad4ca4a4523a27aa5dbc430b
sano <sano@chime.co.jp>
2022-06-11 10:42:02 +0900

    サンプル追加

---
+++ sample/vc/src/a.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+extern int b();
+
+int main(){
+//	printf("hellow wolrd(%d)\n",b());
+	printf("hellow wolrd\n");
+}
---
+++ sample/vc/src/b.cpp
@@ -0,0 +1,12 @@
+#if 0
+#include "b1.h"	//‚Ä‚·‚Æ
+#else
+#include "b2.h"	/* ‚Ä‚·‚Æ */
+#endif
+
+   //‚Ä‚·‚Æ
+
+int b(){
+/*‚Ä‚·‚Æ*/	const char *m="//test";	/*‚Ä‚·‚Æ*/
+	return 123;
+}
